c6e40842ad7731c392a7009dfc196c008582a9bd
VINAY DIVI <divisasisai@gmail.com>
2025-10-14 19:06:28 +0530

    Chat Agent

--- ChatBot_Agent_for_time_and_youtube.py
+++ ChatBot_Agent_for_time_and_youtube.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from semantic_kernel.agents import ChatCompletionAgent, ChatHistoryAgentThread
 from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
 from semantic_kernel.functions import kernel_function
+import requests
 
 import random
 from datetime import datetime
@@ -30,6 +31,36 @@ class TimePlugin:
         current_time = now.strftime("%H:%M:%S")
         
         return current_time
+    @kernel_function(description="to get weather report")
+    def get_weather(self,city):
+        base_url = ""
+        api_key=""
+        city="Bapatla"
+        params = {
+            "key": api_key,
+            "q": city,
+            # you can also include optional params like "aqi": "yes"
+        }
+
+        response = requests.get(base_url, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            # data["location"] has info about the place
+            # data["current"] has weather info
+            loc = data.get("location", {})
+            curr = data.get("current", {})
+
+            name = loc.get("name")
+            region = loc.get("region")
+            country = loc.get("country")
+            temp_c = curr.get("temp_c")
+            condition = curr.get("condition", {}).get("text")
+            humidity = curr.get("humidity")
+            wind_kph = curr.get("wind_kph")
+
+            return f"Weather in {name}, {region}, {country} Condition: {condition} Temperature: {temp_c} °C Humidity: {humidity}% Wind Speed: {wind_kph} kph"
+        else:
+            return f"Failed to fetch weather. Status code: {response.status_code}"
     @kernel_function(description="to open the youtube")
     def open_youtube(self):
       # URL of YouTube
@@ -42,8 +73,8 @@ class TimePlugin:
 
 load_dotenv()
 client = AsyncOpenAI(
-    api_key= "*******",
-    base_url="********",
+    api_key= "",
+    base_url="",
 )
 
 # Create an AI Service that will be used by the `ChatCompletionAgent`
